2c54abf48ecdc8eae44289de0f58774fd1d23e45
Leonxis <leonxis@example.com>
2025-11-16 22:24:58 +0800

    Simplify compile_proto.cmake by removing proxy configuration logic

--- compile_proto.cmake
+++ compile_proto.cmake
@@ -9,49 +9,14 @@ function (compile_proto)
 		set(VENV_BIN_DIR ${VENV}/bin)
 	endif()
 
-	# Check if git proxy is set and extract SOCKS proxy info
-	set(GIT_PROXY "")
-	execute_process(
-		COMMAND git config --global --get http.proxy
-		OUTPUT_VARIABLE GIT_PROXY
-		OUTPUT_STRIP_TRAILING_WHITESPACE
-		ERROR_QUIET
+	add_custom_command(
+		DEPENDS ${CMAKE_SOURCE_DIR}/lib/nanopb/extra/requirements.txt
+		COMMAND ${Python3_EXECUTABLE} -m venv ${VENV}
+		COMMAND ${VENV_BIN_DIR}/pip --disable-pip-version-check install -r ${CMAKE_SOURCE_DIR}/lib/nanopb/extra/requirements.txt
+		COMMAND ${VENV_BIN_DIR}/pip freeze > ${VENV_FILE}
+		OUTPUT ${VENV_FILE}
+		COMMENT "Setting up Python Virtual Environment"
 	)
-
-	# Set up pip install commands with proxy support if needed
-	if(GIT_PROXY)
-		# Extract proxy from git config (format: socks5://host:port)
-		# pip needs http_proxy and https_proxy for SOCKS proxy, and PySocks must be installed first
-		# Use system pip (which already has PySocks) to install PySocks into venv, then use proxy for other packages
-		# Get Python version at configure time to determine site-packages path
-		execute_process(
-			COMMAND ${Python3_EXECUTABLE} -c "import sys; print('{}.{}'.format(sys.version_info.major, sys.version_info.minor))"
-			OUTPUT_VARIABLE PYTHON_VERSION
-			OUTPUT_STRIP_TRAILING_WHITESPACE
-		)
-		set(VENV_SITE_PACKAGES ${VENV}/lib/python${PYTHON_VERSION}/site-packages)
-		add_custom_command(
-			DEPENDS ${CMAKE_SOURCE_DIR}/lib/nanopb/extra/requirements.txt
-			COMMAND ${Python3_EXECUTABLE} -m venv ${VENV}
-			COMMAND ${Python3_EXECUTABLE} -m pip install --target ${VENV_SITE_PACKAGES} PySocks
-			COMMAND ${CMAKE_COMMAND} -E env http_proxy=${GIT_PROXY} https_proxy=${GIT_PROXY} ${VENV_BIN_DIR}/pip install --upgrade pip
-			COMMAND ${CMAKE_COMMAND} -E env http_proxy=${GIT_PROXY} https_proxy=${GIT_PROXY} ${VENV_BIN_DIR}/pip --disable-pip-version-check install -r ${CMAKE_SOURCE_DIR}/lib/nanopb/extra/requirements.txt
-			COMMAND ${VENV_BIN_DIR}/pip freeze > ${VENV_FILE}
-			OUTPUT ${VENV_FILE}
-			COMMENT "Setting up Python Virtual Environment with proxy"
-		)
-	else()
-		add_custom_command(
-			DEPENDS ${CMAKE_SOURCE_DIR}/lib/nanopb/extra/requirements.txt
-			COMMAND ${Python3_EXECUTABLE} -m venv ${VENV}
-			COMMAND ${VENV_BIN_DIR}/pip install --upgrade pip
-			COMMAND ${VENV_BIN_DIR}/pip install PySocks
-			COMMAND ${VENV_BIN_DIR}/pip --disable-pip-version-check install -r ${CMAKE_SOURCE_DIR}/lib/nanopb/extra/requirements.txt
-			COMMAND ${VENV_BIN_DIR}/pip freeze > ${VENV_FILE}
-			OUTPUT ${VENV_FILE}
-			COMMENT "Setting up Python Virtual Environment"
-		)
-	endif()
 
 	set(NANOPB_GENERATOR ${CMAKE_SOURCE_DIR}/lib/nanopb/generator/nanopb_generator.py)
 	set(PROTO_OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR}/proto)
